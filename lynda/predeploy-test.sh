e26782c65e5d4d0cef4b94e0d1f2b99a69ae47c0
Daniel Kang <kangdaniels@gmail.com>
2015-05-27 23:29:37 -0400

    added usage block on function

--- lynda/predeploy-test.sh
+++ lynda/predeploy-test.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 ##
 # This script will deploy to the SDN staging or production environment, after doing some work to tag commits and
@@ -16,6 +15,36 @@
 # Begin script:
 ##
 
+#Usage function
+usage () {
+  echo
+  echo "Usage: $0 [prod:stage] [release branch name] [withvars] [variation]"
+  echo
+  echo '[prod:stage] select either option to choose which Google Apps Engine'
+  echo 'environment to deploy to'
+  echo
+  echo '[release branch name] enter in the release branch version e.g. 2-17-2-A'
+  echo
+  echo '[withvars] this option would be used when you will deploy with branch'
+  echo 'variations'
+  echo
+  echo '[variation] this option you would replace with your variation type e.g.'
+  echo 'A B C D ... etc'
+  echo
+  echo '-----------------------------------------------------------------------'
+  echo 'example 1:'
+  echo 'predeploy to stage with no variation branches'
+  echo "$ $0 stage 2-17-3"
+  echo
+  echo 'example 2:'
+  echo 'predeploy to stage with one variation A branch'
+  echo "$ $0 stage 2-17-3-A withvars A"
+  echo
+  echo 'example 3:'
+  echo 'predeploy to prod with one variation A branch'
+  echo "$ $0 prod 2-17-3-A withvars A"
+}
+
 # Variables
 DATE=$(date +%F" "%H:%M)
 LOCBRANCH=$(git branch)
@@ -29,6 +58,7 @@ DIFF=$(git diff-index --name-only HEAD --)
 # Exit if no arguments are passed on invocation.
 if [  $# -eq 0 ]; then
     echo "No arguments provided"
+    usage
     exit 1
 fi
 
@@ -57,12 +87,14 @@ if [[ $1 = "prod" ]]; then
 fi
 
 # Deploy variations if requested during invocation.
-if [[ $3 = withvars ]]; then
+if [[ $3 = "withvars" ]]; then
 	echo "placeholder for logic to also deploy the variations. WIP."
   case $1 in
-    stage ) echo "this is for stage $4"
+    stage) echo "this is for stage $4"
       ;;
-    prod ) echo "this is for prod $4"
+    prod) echo "this is for prod $4"
+      ;;
+    *) echo "Please enter in withvars"
       ;;
   esac
 fi
